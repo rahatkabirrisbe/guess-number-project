83b29cef197248947d0ddbe6c1cecee5c067fe5b
rahatkabirrisbe <107284570+rahatkabirrisbe@users.noreply.github.com>
2022-06-17 21:42:56 +0600

    added a deafult game changer butto, which can improve playingn

--- app.js
+++ app.js
@@ -8,31 +8,27 @@ const ul = document.querySelector('.history ul')
 const resetBtn = document.querySelector('.btn')
 const maxPlayLimit = document.querySelector('.max-play-limit')
 const maxLimitFixed = document.querySelector('.max-limit')
+const setGame = document.querySelector('.btn-set-game')
+const maxGuessNumber = document.querySelector('.max-guess')
 
 // Data Layer
 let submitted = 0, // 1, 2, 3
     maxSubmittedLimit = 5,
     correctScoreValue = 0,
     wrongScoreValue = 0,
-    gameOver = false;
+    gameOver = false,
+    playBetweenNumber = 10;
 
-
+maxGuessNumber.textContent = playBetweenNumber;
 
 maxLimitFixed.textContent = maxSubmittedLimit;
 
 
 // logical function
-function playAgain(){
-    resetBtn.setAttribute('style', 'display:inline-block')
-        const createdLi = document.createElement('li');
-        createdLi.textContent = 'Game Over! Hit the play again!!'
-        // createdLi.classList.add('play-again')
-        createdLi.setAttribute('style', 'background-color: hsl(7, 90%, 53%)')
-        ul.appendChild(createdLi)
-}
+
 
 function randomNumberMatching(value){
-    let randomNumber = Math.round(Math.random() * 10)
+    let randomNumber = Math.round(Math.random() * playBetweenNumber)
     console.log(randomNumber);
     if(!gameOver && randomNumber === value){
         // alert('Wow! Matched');
@@ -63,6 +59,17 @@ function randomNumberMatching(value){
     }    
 }
 
+function playAgain(){
+    resetBtn.setAttribute('style', 'display:inline-block')
+        const createdLi = document.createElement('li');
+        createdLi.textContent = 'Game Over! Hit the play again!!'
+        // createdLi.classList.add('play-again')
+        createdLi.setAttribute('style', 'background-color: hsl(7, 90%, 53%)')
+        ul.appendChild(createdLi)
+
+}
+
+
 function refreshPage(){
     window.location.reload();
 }
@@ -75,6 +82,27 @@ function maxLimitLive(value){
 let minValue = maxSubmittedLimit;
 maxPlayLimit.innerText = maxSubmittedLimit;
 // Event Listener
+
+setGame.addEventListener('click', ()=>{
+    let playBetween = parseInt(prompt('play between 0 to : (value must be getter than 0)'))
+    if(playBetween > 0){
+        playBetweenNumber = playBetween;
+        maxGuessNumber.textContent = playBetweenNumber;
+    }
+
+    // maximum play limit form user 
+    let setLimitValue = parseInt(prompt('want set play limit? (value must be getter than 0)'))
+    if(setLimitValue>0){
+    maxSubmittedLimit = setLimitValue;
+    maxLimitFixed.textContent = maxSubmittedLimit;
+    minValue = setLimitValue;
+    maxPlayLimit.textContent = minValue;
+    }
+  
+})
+
+
+
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     submitted++;
@@ -106,3 +134,5 @@ form.addEventListener('submit', (e) => {
 resetBtn.addEventListener('click', ()=>{
     refreshPage();
 })
+
+
